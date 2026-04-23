dfcd69b42f9ba517c6212e2d50546b76cd2bbee4
NeruKujimi <NeruKujimi@users.noreply.github.com>
2026-04-23 03:14:49 +0000

    feat: 代码构建报错信息
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ src/components/SegmentEditor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="segment-editor">
+    <!-- 组件内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SegmentEditor'
+}
+</script>
+
+<style lang="scss">
+@import '../styles/_mixins.scss';
+
+.segment-editor {
+  @include input-base();
+  // 其他样式...
+}
+</style>
